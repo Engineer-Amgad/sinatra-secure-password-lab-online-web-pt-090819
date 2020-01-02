02805203f8cb465fdc2c6386b878fdf03e26137e
Amgad Malik <amgadmalik@yahoo.com>
2020-01-02 16:20:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username].empty?
+    if 1params[:username].empty?
       user = User.new(:username => params[:username], :password => params[:password])
 		  if user.save
         redirect "/login"
